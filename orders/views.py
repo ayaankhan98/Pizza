8050ffd6b6f8d4d4be2c6d351234dd3ae5fab2e8
ayaankhan98 <ayaankhan98@gmail.com>
2020-01-02 22:21:38 +0530

    updated comments

--- orders/views.py
+++ orders/views.py
@@ -11,7 +11,10 @@ from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 
+## this view shows the index page of the webapp and the index page shows the
+## resturant menu 
 def index(request):
+    ## context dictionary have all menu items of the resturant
     context = {
         'RegularPizzas':RegularPizza.objects.all(),
         'SilicianPizzas':SilicianPizza.objects.all(),
@@ -26,24 +29,34 @@ def index(request):
     return render(request,"orders/index.html",context)
 
 
+## signup_view views just simply renders the signup page where
+## users can create a new account
 def signup_view(request):
+    ## if a user is not already logged in only then render signup page 
     if not request.user.is_authenticated:
         return render(request,"orders/signup.html")
+    ## else redirect the user to the index page for browsing the Resturant Menu
     return HttpResponseRedirect(reverse("index"))
 
 
+## when the user successfully submits the signup form then this view takes the
+## information given by the user and creates a new user based on that information
 def auth(request):
-    username = request.POST["username"]
-    first_name = request.POST["first_name"]
-    last_name = request.POST["last_name"]
-    email = request.POST["email"]
-    password = request.POST["password"]
-    cnf_password = request.POST["cnf_password"]
-    if(password == cnf_password):
+    username = request.POST["username"] ## getting the username from the form
+    first_name = request.POST["first_name"] ## getting the first name of user
+    last_name = request.POST["last_name"] ## getting the last name of user
+    email = request.POST["email"] ## getting the email id
+    password = request.POST["password"] ## getting the  password  of user
+    cnf_password = request.POST["cnf_password"] ## again getting the password just for conformation
+    if(password == cnf_password): ## checking if and only if both passwords are same 
         try:
+            ## created a new user if passwords matched
             User.objects.create_user(username=username,first_name=first_name,last_name=last_name,email=email,password=password)
         except IntegrityError:
+            ## else if a user with same username exist then return the user to the
+            ## signup page again
             return render(request,"orders/signup.html",{'message':f'User with Username {username} alredy exist !'})
+    ## if both the passwords do not matched then redirect the user to the signup page again
     return HttpResponse("User Not Created")
 
 
